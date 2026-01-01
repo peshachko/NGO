e2aeef48e4f1281e79fd874c897a9140b329337b
Dimitar Dimitrov <mail.mitko@gmail.com>
2026-01-01 18:40:16 +0100

    Добавяне на Учредителен протокол

--- docs/.dir-locals.el
+++ docs/.dir-locals.el
@@ -1,4 +1,4 @@
 ;;; Directory Local Variables            -*- no-byte-compile: t -*-
 ;;; For more information see (info "(emacs) Directory Variables")
 
-((nil . ((jinx-dir-local-words . "ЗЮЛНЦ НЕСТОПАНСКА ПЕШАЧКО Чл ал волеизявление неправителствени нестопанска"))))
+((nil . ((jinx-dir-local-words . "ЗЮЛНЦ НЕСТОПАНСКА ПЕШАЧКО Чл ал волеизявление неправителствени нестопанска протоколчик"))))
